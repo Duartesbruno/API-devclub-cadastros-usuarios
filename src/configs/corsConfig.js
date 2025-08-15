a1f8e4210b4110194fe0641e472e02ab3b799445
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-15 18:31:57 -0300

    Configurando um novo CORS para Workspace

--- src/configs/corsConfig.js
+++ src/configs/corsConfig.js
@@ -3,8 +3,8 @@ import cors from 'cors'
 // cors origin validations
 const isProduction = process.env.NODE_ENV === 'production';
 const allowedOrigins = isProduction
-  ? [process.env.PRODUCTION_ORIGIN]
-  : [process.env.LOCAL_ORIGIN || 'http://localhost:5173', process.env.PRODUCTION_ORIGIN].filter(Boolean);
+  ? [process.env.PRODUCTION_ORIGIN || process.env.PRODUCTION_ORIGIN].filter(Boolean)
+  : [process.env.LOCAL_ORIGIN || 'http://localhost:5173', process.env.PRODUCTION_WORKSPACE_ORIGIN].filter(Boolean);
 
 if (isProduction && !process.env.PRODUCTION_ORIGIN) {
   console.error('Erro: PRODUCTION_ORIGIN não está definido no ambiente Render.');
