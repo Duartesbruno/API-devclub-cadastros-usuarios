6933dcfa8a1df10366432385cfcdd27a41481a92
Bruno.Duarte <bruno.duarte@zaffari.com.br>
2025-08-15 19:17:10 -0300

    Alteracao no CORS

--- src/configs/corsConfig.js
+++ src/configs/corsConfig.js
@@ -2,14 +2,22 @@ import cors from 'cors'
 
 // cors origin validations
 const isProduction = process.env.NODE_ENV === 'production';
-const allowedOrigins = isProduction
-  ? [process.env.PRODUCTION_ORIGIN || process.env.PRODUCTION_WORKSPACE_ORIGIN].filter(Boolean)
-  : [process.env.LOCAL_ORIGIN || 'http://localhost:5173', 
-    process.env.PRODUCTION_ORIGIN || process.env.PRODUCTION_WORKSPACE_ORIGIN].filter(Boolean);
 
 if (isProduction && !process.env.PRODUCTION_ORIGIN) {
   console.error('Erro: PRODUCTION_ORIGIN não está definido no ambiente Render.');
   process.exit(1);
 }
+
+if (!isProduction && !process.env.LOCAL_ORIGIN) {
+  console.warn(
+    'Atenção: LOCAL_ORIGIN não está definido. Usando http://localhost:5173 como fallback.'
+  );
+}
+
+const allowedOrigins = [
+  !isProduction ? process.env.LOCAL_ORIGIN || 'http://localhost:5173' : null,
+  process.env.PRODUCTION_ORIGIN,
+  process.env.PRODUCTION_WORKSPACE_ORIGIN,
+].filter(Boolean);
 
 export const corsMiddleware = cors({ origin: allowedOrigins });
